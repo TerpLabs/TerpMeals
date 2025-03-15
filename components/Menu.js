5338cc8d1139f955ba3a160874dde6bdbf1a7c72
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-03-14 22:25:28 -0400

    Add files via upload

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,4 @@
-import {Text, View, ScrollView, StyleSheet, TouchableOpacity, Animated } from 'react-native'
+import {Text, View, ScrollView, StyleSheet, TouchableOpacity, Animated, Image, Touchable } from 'react-native'
 import { useState, useEffect, useRef } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNPickerSelect from 'react-native-picker-select';
@@ -61,18 +61,35 @@ export default ({navigation}) => {
         <ScrollView className='bg-white'>
 
             
-            <Text className = "text-3xl font-bold">Today's Menu</Text>
-            <RNPickerSelect 
-                onValueChange={setSelectedHall}
-                items = {[
-                    { label: 'Yahentamitsi', value: 'Y' },
-                    { label: '251 North', value: 'North' },
-                    { label: 'South Campus Dining', value: 'South' },
-                ]}
-            />
-            <Text>{"\n"}</Text>
+            <View className = "bg-red-500 border-b-4 border-red-800 px-4 py-4">
+                <Text className = "text-4xl font-bold text-white mb-4">Today's Menu</Text>
+                <View className = "flex-row">
+                    {[
+                    { label: 'Y', value: 'Y' },
+                    { label: '51', value: 'North' },
+                    { label: 'S', value: 'South' },
+                ].map((diningHall, id) => {
+                    return  (
+                        <TouchableOpacity
+                        className = {`italic border-b-4 mx-4 ${
+                           selectedHall == diningHall.value ? "border-white text-yellow-300" : "border-red-800 text-white"
+                        } font-bold text-xl rounded-md w-8 text-center transition-all`}
+                        onPress={() => {
+                            setSelectedHall(diningHall.value)
+                            slideAnim.setValue(0)
+                            Animated.timing(slideAnim, {
+                                toValue: 1,
+                                duration: 1000,
+                                useNativeDriver: false, 
+                              }).start()
 
-            {Object.keys(data).includes(selectedHall) && <Text className = "text-lg font-bold">Menu for {data[selectedHall].name}</Text>}
+                        }}>{diningHall.label}</TouchableOpacity>
+                    )
+                })}
+                </View>
+            </View>
+
+            {Object.keys(data).includes(selectedHall) && <Text className = "text-3xl px-4 font-bold">{data[selectedHall].name}</Text>}
             
             {Object.keys(data).includes(selectedHall) && Object.keys(data[selectedHall]).map((meal, i) => {
                 if(meal != "name" && meal != "link"){
@@ -86,13 +103,18 @@ export default ({navigation}) => {
                                 return (
                                     <View>
                                         <Text className = "italic text-lg px-4 my-8 font-semibold">{dish}</Text>
-                                        <View className = "">
+                                        <View>
                                             {Object.keys(data[selectedHall][meal][dish]).map((item, idy) => {
                                                 return (
                                                     <View className = "h-32 py-2 border-b-4 border-red-500 w-full rounded-2xl px-12 inline">
-                                                        <View className = "flex-col float-left">
-                                                            <Text className='font-bold text-black italic text-xl w-56'>{item.replace("&amp;", "and")}</Text>
-                                                            <View className = 'flex-row mt-4'>
+                                                        <View className = "flex-col float-left w-72">
+                                                            <Text className='font-bold text-black italic text-xl'>{item.replace("&amp;", "and")}</Text>
+                                                            <View className = 'flex-row flex-wrap mt-4'>
+                                                                {data[selectedHall][meal][dish][item].map((tag, idz) => {
+                                                                    return (
+                                                                        <Image source = {tag[1]} className = "mx-2 my-2 w-6 h-6"></Image>
+                                                                    )
+                                                                })}
                                                             </View>
                                                         </View>
                                                         
@@ -100,7 +122,7 @@ export default ({navigation}) => {
 
                                                         {Object.keys(nut[item]).includes('calories_per_serving') && 
                                                             <View className = "flex-row float-left items-centers">
-                                                                <Text className = 'font-bold text-xl px-4 bg-red-500 w-32 h-24 border-red-700 border-4 text-white mx-4 text-center'
+                                                                <Text className = 'font-bold text-xl px-4 w-32 h-24 text-red-600 mx-4 text-center'
                                                                 >{`Cal: ${nut[item]['calories_per_serving']}`}   </Text>
 
                                                                 <View className = 'float-right flex-col border-l-2 border-black'>
@@ -162,6 +184,16 @@ export default ({navigation}) => {
 
                                                                 </View>
 
+                                                                </View>
+                                                                <View className = "w-24 h-full"></View>
+
+                                                                <View style={{ flexDirection: 'col', justifyContent: 'flex-end' }}>
+                                                                    <TouchableOpacity className = "text-5xl bold italic text-gray-400 my-2 float-right">
+                                                                        <Image source = {require('assets/add.png')}></Image>
+                                                                    </TouchableOpacity>
+                                                                    <TouchableOpacity className = "text-5xl bold italic text-gray-400 my-2 float-right">
+                                                                        <Image source ={require('assets/heart.png')}></Image>
+                                                                    </TouchableOpacity>
                                                                 </View>
                                                             </View>
                                                         }
